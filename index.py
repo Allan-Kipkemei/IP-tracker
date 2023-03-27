2832fc38b8744c4cf6cc1787317b6feb8a3195a2
Allan Kiplagat <allankiplgat@gmail.com>
2023-03-27 17:38:43 +0300

    first commit

---
+++ index.py
@@ -0,0 +1,21 @@
+import geo
+
+ip = geo.getIP()
+print(ip)
+
+country =geo.getCountry(ip, 'plain')
+print(country)
+
+country= geo.getCountry(ip, 'json')
+print(country)
+
+geoData =geo.getGeoData(ip)
+print(geoData)
+
+ptrData = geo.getPTR(ip)
+
+print(ptrData)
+
+geo.showIpDetails('')
+
+geo.showCountryDetail('')
